847f24f7d2b00bb475750f0cfac1e3e77a4ae952
Niklas Dillenberger <niklasdillenberger@niklass-macbook-air.local>
2018-11-14 11:10:30 +0100

    Initial Commit

---
+++ PRG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG
+//
+//  Created by Niklas Dillenberger on 14.11.18.
+//  Copyright © 2018 Niklas Dillenberger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
